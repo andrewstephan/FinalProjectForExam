1110469a84877e4ef539542288527cb9cc2a4c23
andrewstephan <stefanmandrei95@gmail.com>
2023-07-22 23:31:30 +0200

    Update.

--- src/test/java/ro/mediagalaxy/tests/MainTest.java
+++ src/test/java/ro/mediagalaxy/tests/MainTest.java
@@ -14,6 +14,7 @@ public class MainTest {
         //open page
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
 
